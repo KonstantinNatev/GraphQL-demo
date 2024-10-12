149a104cb3a9a17a1f826985afec8d23da53f4c0
Konstantin-Natev <76740873+KonstantinNatev@users.noreply.github.com>
2024-10-12 11:55:48 +0300

    Create new Types and getting data from json files and  represent it with GraphQL

--- server.js
+++ server.js
@@ -1,30 +1,62 @@
-const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
 const {
     GraphQLSchema,
     GraphQLObjectType,
+    GraphQLList,
+    GraphQLInt,
+    GraphQLNonNull,
     GraphQLString
-} = require('graphql');
-import authors from "./data/author.json";
-import books from "./data/books.json";
+} = require("graphql");
+const {books} = require("./data/books.json");
+const {authors} = require("./data/authors.json");
 
 const app = express();
 
-const schema = new GraphQLSchema({
-    query: new GraphQLObjectType({
-        name: 'Hello',
-        fields: () => ({
-            message: { 
-                type: GraphQLString,
-                resolve: () => 'Hello Guys'
-            }
-        })
+const AuthorType = new GraphQLObjectType({
+    name: "Author",
+    description: "This is author of the book",
+    fields: () => ({
+        id: { type: GraphQLNonNull(GraphQLInt) },
+        name: { type: GraphQLNonNull(GraphQLString) }
     })
 });
 
-app.use('/graphql', graphqlHTTP({
+const BookType = new GraphQLObjectType({
+    name: "Book",
+    description: "This is represent a book by the author",
+    fields: () => ({
+        id: { type: GraphQLNonNull(GraphQLInt) },
+        name: { type: GraphQLNonNull(GraphQLString) },
+        authorId: { type: GraphQLNonNull(GraphQLInt) },
+        author: { 
+            type: AuthorType,
+            resolve: (book) => {
+                return authors.find((author) => author.id === book.authorId)
+            }
+        }
+    })
+});
+
+const RootQueryType = new GraphQLObjectType({
+    name: "Query",
+    description: "",
+    fields: () => ({
+        books: {
+            type: GraphQLList(BookType),
+            description: "List of all Books",
+            resolve: () => books
+        }
+    })
+});
+
+const schema = new GraphQLSchema ({
+    query: RootQueryType
+});
+
+app.use("/graphql", graphqlHTTP({
     schema: schema,
     graphiql: true
 }));
 
-app.listen(5000, () => console.log('Server Running on port 5000'))
+app.listen(5000, () => console.log("Server Running on port 5000"));
